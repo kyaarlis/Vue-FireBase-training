4e118bdff139599da43b7a3fce16257076e3fe1a
Karlis Veckagans <veckaganskarlis@gmail.com>
2023-05-27 20:53:29 +0300

    first commit

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: []
+    }
+  },
+  methods: {
+    loadExperiences() {
+      fetch('https://vue-hhtp-demo-71d8c-default-rtdb.europe-west1.firebasedatabase.app/surveys.json').then((response) => {
+        if (response.ok) {
+          return response.json()
+        }
+      })
+      .then((data) => {
+        const results = []
+        for (const id in data) {
+          results.push({
+            id: id, 
+            name: data[id].name, 
+            rating: data[id].rating})
+        }
+        this.results = results
+      })
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
